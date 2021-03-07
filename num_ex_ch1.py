a8aab52e84e33a69dee4ee640d543fd83e82f1bb
Sneha Sinha <ssinha1.314@gmail.com>
2021-03-07 14:47:45 +0000

    finish idc stability

--- num_ex_ch1.py
+++ num_ex_ch1.py
@@ -1,91 +1,64 @@
+# chapter 1 graphs
 import numpy as np
 from matplotlib import pyplot as plt
+from pandas import DataFrame as DF
 
+from scipy.interpolate import lagrange
 import test_examples as ex
 from dc_experiments import DCs as dc
 from serial_examples import Schemes as scheme
 
 
-def analyse_scheme(dy_dt, y, a, b, ics, step_sizes, order_idc):
-    max_global_err = []
-    for i in range(len(step_sizes)):
-        h = step_sizes[i]
-        y_pred = scheme(v_field=dy_dt,
-                        start=a, stop=b, h=h, init_conditions=ics)
-
-        times = np.arange(a, b, h)
-        exact = [y(t) for t in times]
-        y_T = y(b)
-
-        df = d_f({'times': times,
-                  'euler': y_pred.euler()[1],
-                  'rk4':  y_pred.rk4()[1],
-                  'ab4': y_pred.ab4()[1],
-                  'exact': exact})
-        idc_err = []
-        for order in order_idc:
-            ts, df['idc_' + str(order)] = dc().idc_fe(a=a, b=b,
-                                                      alpha=ics, N=int((b-a)/h), p=order, f=dy_dt)
-            #ts, df['idc_' + str(order)] = IDC(a=a, b=b, N=int((b-a)/h), p =order, y0=ics)  #
-            df['exact_'+str(order)] = [y(t) for t in ts]
-            idc_err = idc_err + \
-                [df['exact_'+str(order)].sub(df['idc_' +
-                                                str(order)]).abs().max()]
-
-        df['error_euler'] = df['exact'].sub(df['euler']).abs()
-        df['error_rk4'] = df['exact'].sub(df['rk4']).abs()
-        df['error_ab4'] = df['exact'].sub(df['ab4']).abs()
-        max_global_err.append(idc_err+[df['error_euler'].max(),
-                                       df['error_rk4'].max(),
-                                       df['error_ab4'].max()])
-    return max_global_err
-
-
 def orders():
+    y0 = 1
+    y = ex.y_exact0
+    dy_dt = ex.func0
     def analyse_scheme(dy_dt, y, a, b, ics, step_sizes, order_idc):
-        err = []
-        y_t = y(b)
-        def rel_err(y_p): return np.abs(y_t - y_p) / np.abs(y_t)
-
+        max_global_err = []
         for i in range(len(step_sizes)):
             h = step_sizes[i]
             y_pred = scheme(v_field=dy_dt,
                             start=a, stop=b, h=h, init_conditions=ics)
 
-            err_fe = rel_err(y_pred.euler()[1][-1])
-            err_rk4 = rel_err(y_pred.rk4()[1][-1])
-            err_ab4 = rel_err(y_pred.ab4()[1][-1])
-
-            h_errs = [err_fe, err_rk4, err_ab4]
-
+            times = np.arange(a, b, h)
+            exact = [y(t) for t in times]
+            
+            df = DF({'times': times,
+                        'euler': y_pred.euler()[1],
+                        'rk4':  y_pred.rk4()[1],
+                        'ab4': y_pred.ab4()[1],
+                        'exact': exact})
+            idc_err = []
             for order in order_idc:
-                y_idc = dc().ridc_fe(a=a, b=b, alpha=ics, N=int(
-                    (b-a)/h), p=order, K=order-1, f=dy_dt)[0]
-                h_errs = h_errs + [rel_err(y_idc[-1])]
+                ts, df['idc_' + str(order)] = dc().idc_fe(a=a, b=b, alpha=y0, N=int((b-a)/h), p=order, f=dy_dt)
+                df['exact_'+str(order)] = [y(t) for t in ts]
+                idc_err = idc_err + [df['exact_'+str(order)].sub(df['idc_' + str(order)]).abs().max()]
+                
+            df['error_euler'] = df['exact'].sub(df['euler']).abs()
+            df['error_rk4'] = df['exact'].sub(df['rk4']).abs()
+            df['error_ab4'] = df['exact'].sub(df['ab4']).abs()
+            max_global_err.append(idc_err+[df['error_euler'].max(),
+                                        df['error_rk4'].max(),
+                                        df['error_ab4'].max()])
 
-            #print(h, h_errs)
+        return max_global_err
 
-            err.append(h_errs)
+    h_sizes = [10**i for i in [-1, -2, -3, -4]]
 
-        return np.array(err)
-
-    h_sizes = [10**i for i in [-1, -2, -3]]  # , -3, -4]]
-
-    start, stop = 0, 10
+    start, stop = 0, 1
 
     N = [1/h for h in h_sizes]
 
-    max_global_err = analyse_scheme(
-        ex.func0, ex.y_exact0, start, stop, 1, h_sizes, [3, 6, 11])
+    max_global_err = analyse_scheme(dy_dt, y, start, stop, y0, h_sizes, [3,6,11])
 
     plt.plot(N, max_global_err)
 
+    plt.plot(N[:-1],max_global_err[:-1])
     plt.yscale('log')
     plt.xscale('log')
-    plt.ylabel('log Relative Error')
+    plt.ylabel('log Maximum Global Error')
     plt.xlabel('log Number of time-steps')
-    plt.legend(['Forward Euler (order 1)', 'Runge-Kutta (order 4)', 'Adam Bashforth (order 4)',
-                'IDC(3)-FE', 'IDC(6)-FE', 'IDC(11)-FE', ], loc='best', bbox_to_anchor=(1, 0.7))
+    plt.legend(['IDC(3)-FE', 'IDC(6)-FE', 'IDC(11)-FE','Forward Euler (order 1)', 'Runge-Kutta (order 4)','Adam Bashforth (order 4)', ], loc='centre right', bbox_to_anchor=(1, 0.7))
     plt.show()
 
 # 1 step
@@ -167,7 +140,7 @@ def tests(i):
         def sigma(z): return 1
         stability_mstep(rho, sigma, [-2, 2, -2, 2])
 
-# !idc-stab, does not work
+# !stability_dc, does not work
 
 def stability_dc(axisbox=[-2, 2, -2, 2]):
     # idc-stability
@@ -184,7 +157,7 @@ def stability_dc(axisbox=[-2, 2, -2, 2]):
     K = [5]
     for k in K:
         def r(z):
-            def func_lambda(t, y): return stab_func(z, t, y)
+            def func_lambda(t, y): return stab_func(z, y)
             return abs(max(dc().ridc_fe(0, 1, 1, 20, k+1, k, func_lambda)[0]))
 
         Rabs = np.array([[r(zx) for zx in zy] for zy in Z])[:, :, 0]
@@ -219,6 +192,9 @@ def idc_stab(M):
             reals_p = dc().idc_stability(-i, m, f)
             if abs(reals_p[-1]) <= 1.:
                 reals_in_range.append(round(-i, 6))
+                print(i)
+            else:
+                break
 
         # finding the maximum imaginary values correcsponding to 
         # the real ones that satisfy the amplification factor
@@ -228,7 +204,8 @@ def idc_stab(M):
                 ims_p = dc().idc_stability(complex(r, i), m, f)
                 if abs(ims_p[-1]) <= 1.:
                     goodlistIm1.append(round(i, 6))
-           
+                else:
+                    break
             imgs_in_range.append(round(max(goodlistIm1), 6))
 
         l = list(zip(reals_in_range,imgs_in_range))
@@ -250,33 +227,91 @@ def idc_stab(M):
     plt.show()
 
 
-def ridc_stab(M):
+def RIDCtesteq(M,K,l, testeq):#Euler
+    #(a,b)-endpoints, N-number of steps, p-order of method, K- No. intervals,  y0-I.C, F-function
+     
+    J = int(10/K)
+    sol_list = np.zeros(11,dtype = complex) #stores the solution N+1
+    sol_list[0] = 1.
+    Y = np.zeros((J,K+1),dtype = complex)  #approx solution
+    Y1 = np.zeros((J,K+1),dtype = complex)   #corrected solution
+    Y[0,0]= 1.           #inital value
+    S = np.zeros((M,M+1))  #integration matrix
+    
+    for m in range(M):   # calculating integration matrix
+        for i in range(M+1):
+            x = np.arange(M+1)  # Construct a polynomial
+            y = np.zeros(M+1)   # which equals 1 at i, 0 at other points
+            y[i] = 1
+            p = lagrange(x, y)  # constructs polynomial
+            para = np.poly1d.integ(p)  
+            S[m,i] = para(m+1) - para(m)  #finds definite integral of polynomial and adds to integral matrix
+            
+    for j in range(J):
+        Y[:, 0] = sol_list[j*K]  # predictor starts w last point in j-1 interval
+        for m in range(K):   #prediction
+            Y[j,m+1] = Y[j,m] + testeq(l,Y[j,m]) #Eulers forward method  
+            
+        for l1 in range(1,M+1):   #correction
+            Y1[j,0] = Y[j,0]
+            
+            for m in range(M):  
+                
+                g = sum([S[m,k]*testeq(l,Y[j,k]) for k in range(M+1)])
+
+                Y1[j,m+1] = Y1[j,m] + testeq(l,Y1[j,m])-testeq(l,Y[j,m]) + g #solve error equation with forward Euler
+            
+            for m in range(M,K):
+                
+                g = sum([S[M-1,k]*testeq(l,Y[j,m-M+k+1]) for k in range(M+1)])
+                
+                Y1[j,m+1] = Y1[j,m] + (testeq(l,Y1[j,m])-testeq(l,Y[j,m])) + g  #solve error equation with forward Euler
+                    
+            Y[j,:] = Y1[j,:]
+    
+        sol_list[j*K+1:j*K+K+1] = Y1[j,1:K+1]
+
+            
+    return sol_list
+
+
+def ridc_stab(M_):
     # *Author: Sam Brossler
     f = ex.stab_eq
     col = ['y','g','c','m']
     lines = []
     fig, ax = plt.subplots(1, 1, figsize=(9, 6))
-    for I, m in enumerate(M):    
-        reals_in_range = []
-        imgs_in_range = []
-        # Finding the real values that for the stability interval
-        for i in np.arange(0, 3.01, 0.01):
-            reals_p = dc().ridc_fe_stab(4,10,-i,f)
-            if abs(reals_p[-1]) <= 1.:
-                reals_in_range.append(round(-i, 6))
-
-        # finding the maximum imaginary values correcsponding to 
-        # the real ones that satisfy the amplification factor
-        for r in reals_in_range:
+    tests(3)
+    for I, m_ in enumerate(M_):    
+        goodlistRe = []
+        goodlistIm = []
+        #Finding the real values that for the stability interval
+        for i in np.arange(0,2.005,0.005):
+            M = RIDCtesteq(m_,10,-i,f)
+            df = [abs(M[j]) for j in range(1,11)]
+            r = max(df)
+            if r<=1.:
+                goodlistRe.append(round(-i,6))
+        
+        print('done reals')
+        #print(goodlistRe)
+        #finding the maximum imaginary values correcsponding to the real ones that satisfy the amplification factor
+        for m in goodlistRe:
             goodlistIm1 = []
-            for i in np.arange(0, 3.01, 0.01):
-                ims_p = dc().ridc_fe_stab(4,10,-i,f)
-                if abs(ims_p[-1]) <= 1.:
-                    goodlistIm1.append(round(i, 6))
-           
-            imgs_in_range.append(round(max(goodlistIm1), 6))
+            for i in np.arange(0,1.005,0.005):
+                M = RIDCtesteq(m_,10,complex(m,i),f)
+                #print(M)
+                dl = [abs(M[j]) for j in range(1,11)]
+                #print(df)
+                r = max(dl)
+                #print(r)
+                if r<=1.:
+                    goodlistIm1.append(round(i,6))
+            something = max(goodlistIm1)
+            goodlistIm.append(round(something,11))
+        print('done ims')
 
-        l = list(zip(reals_in_range,imgs_in_range))
+        l = list(zip(goodlistRe,goodlistIm))
 
         xs = np.array([x[0] for x in l])
         ys = np.array([x[1] for x in l])
@@ -284,11 +319,11 @@ def ridc_stab(M):
         l = plt.plot(xs,ys,col[int(I)])
         lines = lines + [l]
         plt.plot(xs, -ys, col[int(I)])
-        print('done m =', m)
+        print('done m =', m_)
     plt.savefig('ridc_stab_M.png')
     plt.title('Stability regions for RIDC for varying M')
-    st = lambda m: 'RIDC(4,10)_fe M = ' + str(m)
-    plt.legend([l[0] for l in lines], map(st,M))
+    st = lambda m: 'RIDC(M,10)_fe M = ' + str(m)
+    plt.legend([l[0] for l in lines], map(st,M_))
     plt.xlabel(r'$Re(\lambda \Delta t)$')
     plt.ylabel(r'$Im(\lambda \Delta t)$')
     plt.savefig('ridc_stab_M.png')
@@ -297,6 +332,8 @@ def ridc_stab(M):
 
 if __name__ == '__main__':
     # tests(2)
-    #idc_stab([2, 3, 5, 6])
-    ridc_stab([2, 3, 5, 6])
+    #idc_stab([2])
+    ridc_stab([2, 3, 4])
+
+    #orders()
     
